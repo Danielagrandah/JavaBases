f7bf97c77f387e363ab1f752e67230db78ea5136
Karen Daniela GRANDA <karen.grandah@gmail.com>
2024-10-29 10:05:04 +0100

    Create switch.java
    Estructuras de switch para java

---
+++ switch.java
@@ -0,0 +1,11 @@
+switch (expresion) {
+  case valor1:
+    // código a ejecutar si la expresión es igual a valor1
+    break;
+  case valor2:
+    // código a ejecutar si la expresión es igual a valor2
+    break;
+  // otros casos posibles...
+  default:
+    // código a ejecutar si ninguno de los casos anteriores se cumple
+} 
